6b624697e9ef95b208421fa92efa14a892a86336
Steven Loaiza <steven.loaiza@outlook.com>
2025-05-10 01:04:36 -0400

    Methods return dataframe

--- libs/linguafuse/errors.py
+++ libs/linguafuse/errors.py
@@ -1,16 +1,16 @@
 import pandas as pd
-from typing import Bool
 
-def validate_columns(v: pd.DataFrame) -> Bool:
+def validate_columns(v: pd.DataFrame) -> pd.DataFrame:
     required_columns = {'text', 'encoded_label', 'label'}
     missing_columns = required_columns - set(v.columns)
     if missing_columns:
         raise ValueError(f"DataFrame is missing required columns: {missing_columns}")
-    return True
+    return v
 
-def validate_encodings(v: pd.DataFrame) -> Bool:
+def validate_encodings(v: pd.DataFrame) -> pd.DataFrame:
+    print(v)
     range_values = v['encoded_label'].min(), v['encoded_label'].max()
     max_len = v['label'].unique().size
     assert range_values == (0, max_len - 1), f"Encoded labels should be in the range [0, {max_len - 1}] but got {range_values}"
 
-    return True
+    return v
