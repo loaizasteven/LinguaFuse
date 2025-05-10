97f417cdf963eaf23cafa4aa0bf0e88667a39d2f
Steven Loaiza <steven.loaiza@outlook.com>
2025-05-10 00:34:14 -0400

    Add placeholder aws connection

---
+++ libs/linguafuse/aws/connections.py
@@ -0,0 +1,8 @@
+from typing import Union
+import os
+
+# TODO: This function is a placeholder and should be implemented in the future for AWS S3 bucket connectivity.
+
+def get_weights_path(*args, **kwargs) -> Union[str, os.PathLike]:
+    raise NotImplementedError("This function is not implemented yet.")
+    
