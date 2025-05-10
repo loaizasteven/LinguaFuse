c92d2b423e3ed5c0089e949eebc268e4fb41b0f2
Steven Loaiza <steven.loaiza@outlook.com>
2025-05-10 01:06:19 -0400

    Update return df method to use conenction mng

--- libs/linguafuse/framework.py
+++ libs/linguafuse/framework.py
@@ -2,16 +2,26 @@ from pydantic import BaseModel, Field
 from typing import Union
 import os
 
-from linguafuse.loader import ProcessedDataset
+from linguafuse.loader.dataset import ProcessedDataset
 import pandas as pd
+
+from cloud import (
+    Scope,
+    ConnectionManager,
+)
 
 class FineTuneOrchestration(BaseModel):
     """ Orchestration for E2E fine-tuning. """
     path: Union[str, os.PathLike] = Field(..., description="Path or URI to the dataset.")
-
+    scope: str = Field(
+        default=Scope.LOCAL,
+        description="The scope of the orchestration, indicating the cloud service it connects to."
+    ) 
     def _return_dataset(self):
         """ Returns the dataset. """
-        data = pd.read_csv(self.path)
+        conn = ConnectionManager(scope=self.scope, asset_details=self.path)
+        conn.connect()
+        data = conn.read_pandas()
         if data.empty:
             raise ValueError("The dataset is empty.")
         return ProcessedDataset(data=data)
