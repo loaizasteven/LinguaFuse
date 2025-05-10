08b4ab4033f39223d37b6aa72f352abeff273eae
Steven Loaiza <steven.loaiza@outlook.com>
2025-05-10 01:06:33 -0400

    Remove print statement

--- libs/linguafuse/errors.py
+++ libs/linguafuse/errors.py
@@ -8,7 +8,6 @@ def validate_columns(v: pd.DataFrame) -> pd.DataFrame:
     return v
 
 def validate_encodings(v: pd.DataFrame) -> pd.DataFrame:
-    print(v)
     range_values = v['encoded_label'].min(), v['encoded_label'].max()
     max_len = v['label'].unique().size
     assert range_values == (0, max_len - 1), f"Encoded labels should be in the range [0, {max_len - 1}] but got {range_values}"
