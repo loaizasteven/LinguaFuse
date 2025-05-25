daf1822c083af62c02b8d6f889b2ad1a55dc4a4a
Steven Loaiza <steven.loaiza@outlook.com>
2025-05-24 20:21:29 -0400

    Tf provision s3 bucket

--- infra/main.tf
+++ infra/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 5.83"
     }
   }
 
@@ -19,5 +19,25 @@ resource "random_pet" "storage" {
   keepers = {
     # Generate a new pet name each time we switch to a new s3 id
     s3_id = var.s3_instance_name
+  }
+  prefix = "s3-linguafuse"
+}
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = random_pet.storage.id
+  tags   = {
+    Name        = "s3-${random_pet.storage.id}"
+    Environment = var.s3_instance_name
+    Project     = "LinguaFuse"
+  }
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
   }
 }
